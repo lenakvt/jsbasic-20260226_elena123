2c2583d504610ecc72ec555e62bac6d2e030700c
Elena Kilovataya <e.kilovataya@truestats.ru>
2026-03-11 18:43:18 +0400

    Реализует sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,19 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  let keys = Object.keys(salaries); 
+  for (let i = 0; i < keys.length; i++) {
+    let keyname = keys[i]; 
+
+    if (typeof salaries[keyname] === 'number' 
+      && !isNaN(salaries[keyname]) 
+      && salaries[keyname] !== Number.POSITIVE_INFINITY
+      && salaries[keyname] !== Number.NEGATIVE_INFINITY
+    ) {
+      result = result + salaries[keyname];
+    }
+
+  }
+  return result;
 }
+
+

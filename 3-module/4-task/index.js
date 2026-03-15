1a8d54bd31e7b607ee8c88c0e88100b366d461e1
Elena Kilovataya <e.kilovataya@truestats.ru>
2026-03-15 17:24:51 +0400

    Реализация showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filterdusers = users.filter(user => {
+    if (user.age <= age) { return true; }
+    return false;
+  });
+
+  let balances = filterdusers.map(user => {
+    return user.name + ', ' + user.balance;
+  });
+  return balances.join('\n');
 }
+

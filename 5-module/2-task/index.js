e9177365f259a02cddf3d78d64db6bc6585cfd19
Elena Kilovataya <e.kilovataya@truestats.ru>
2026-03-22 18:24:56 +0400

    Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector ('#text');
+  btn.addEventListener('click', () => {
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');  // Показать текст
+    } else {
+      text.setAttribute('hidden', '');  // Скрыть текст
+    }
+  });
 }
+

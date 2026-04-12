f366b4b52da0e17e141ba0234809b8966d73cce6
Elena Kilovataya <e.kilovataya@truestats.ru>
2026-04-12 14:47:27 +0400

    Реализация списка товаров

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,70 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    // Создаём структуру сетки товаров
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `);
+
+    this.inner = this.elem.querySelector('.products-grid__inner');
+
+    // Отрисовываем все товары
+    this.render();
+  }
+
+  render() {
+    // Очищаем существующие карточки
+    this.inner.innerHTML = '';
+
+    // Получаем отфильтрованные товары
+    let filteredProducts = this.getFilteredProducts();
+
+    // Создаём и добавляем карточку для каждого товара
+    filteredProducts.forEach(product => {
+      let card = new ProductCard(product);
+      this.inner.appendChild(card.elem);
+    });
+  }
+
+  updateFilter(newFilters) {
+    // Пропускаем текущие фильтры с новыми
+    this.filters = Object.assign(this.filters, newFilters);
+
+    // Перерисовываем с новыми фильтрами
+    this.render();
+  }
+
+  getFilteredProducts() {
+    return this.products.filter(product => {
+      // Фильтр noNuts: исключаем товары с орехами
+      if (this.filters.noNuts === true && product.nuts === true) {
+        return false;
+      }
+
+      // Фильтр vegeterianOnly: показываем только вегетарианские товары
+      if (this.filters.vegeterianOnly === true && product.vegeterian !== true) {
+        return false;
+      }
+
+      // Фильтр maxSpiciness: показываем товары с остротой <= maxSpiciness
+      if (this.filters.maxSpiciness !== undefined) {
+        let spiciness = product.spiciness || 0;
+        if (spiciness > this.filters.maxSpiciness) {
+          return false;
+        }
+      }
+
+      // Фильтр category: показываем только товары из указанной категории
+      if (this.filters.category && this.filters.category !== '') {
+        if (product.category !== this.filters.category) {
+          return false;
+        }
+      }
+
+      return true;
+    });
   }
 }

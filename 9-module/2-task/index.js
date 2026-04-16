dde94c95ae2dbd0585c2059c6b7000b4fdb2d03c
Elena Kilovataya <e.kilovataya@truestats.ru>
2026-04-16 21:55:52 +0400

    Реализация сборки проекта

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -16,6 +16,67 @@ export default class Main {
   }
 
   async render() {
-    // ... ваш код
+    this.carousel = new Carousel(slides);
+    document.querySelector('[data-carousel-holder]').append(this.carousel.elem);
+
+    this.ribbonMenu = new RibbonMenu(categories);
+    document.querySelector('[data-ribbon-holder]').append(this.ribbonMenu.elem);
+
+    this.stepSlider = new StepSlider({ steps: 5, value: 3 });
+    document.querySelector('[data-slider-holder]').append(this.stepSlider.elem);
+
+    this.cartIcon = new CartIcon();
+    document.querySelector('[data-cart-icon-holder]').append(this.cartIcon.elem);
+
+    this.cart = new Cart(this.cartIcon);
+
+    let response = await fetch('products.json');
+    let products = await response.json();
+
+    this.products = products;
+
+    let productsGridHolder = document.querySelector('[data-products-grid-holder]');
+    productsGridHolder.innerHTML = '';
+
+    this.productsGrid = new ProductsGrid(this.products);
+    productsGridHolder.append(this.productsGrid.elem);
+
+    this.productsGrid.updateFilter({
+      noNuts: document.getElementById('nuts-checkbox').checked,
+      vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
+      maxSpiciness: this.stepSlider.value,
+      category: this.ribbonMenu.value
+    });
+
+    document.body.addEventListener('product-add', (event) => {
+      let product = this.products.find(p => p.id === event.detail);
+      if (product) {
+        this.cart.addProduct(product);
+      }
+    });
+
+    this.stepSlider.elem.addEventListener('slider-change', (event) => {
+      this.productsGrid.updateFilter({
+        maxSpiciness: event.detail
+      });
+    });
+
+    this.ribbonMenu.elem.addEventListener('ribbon-select', (event) => {
+      this.productsGrid.updateFilter({
+        category: event.detail
+      });
+    });
+
+    document.getElementById('nuts-checkbox').addEventListener('change', (event) => {
+      this.productsGrid.updateFilter({
+        noNuts: event.target.checked
+      });
+    });
+
+    document.getElementById('vegeterian-checkbox').addEventListener('change', (event) => {
+      this.productsGrid.updateFilter({
+        vegeterianOnly: event.target.checked
+      });
+    });
   }
 }
